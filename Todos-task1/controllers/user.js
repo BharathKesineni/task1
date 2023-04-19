f6f8ba2521ea89739eaccc81638d2989eccf56e1
BharathKesineni <bharath.kesineni@brainvire.com>
2023-04-19 20:08:00 +0530

    update session

--- Todos-task1/controllers/user.js
+++ Todos-task1/controllers/user.js
@@ -83,20 +83,60 @@ exports.register = (req, res) => {
 	}
 }
 
+// exports.login = (req, res, next) => {
+
+// 	/* Authenticating if login was successful or
+// 	not with the help of passport */
+// passport.authenticate('local', {
+// 	successRedirect: res.send("Login Successful"),
+
+// 	failureRedirect: res.send("Error in Login"),
+// 	failureFlash: false
+// })(req, res, next);
+
+// }
+
+// exports.logout = (req, res) => {
+// 	req.logout();
+// /* Logging out */
+// 	res.send("User Logout");
+// }
+
 exports.login = (req, res, next) => {
-
-	/* Authenticating if login was successful or
-	not with the help of passport */
-passport.authenticate('local', {
-	successRedirect: res.send("Login Successful"),
-	failureRedirect: res.send("Error in Login"),
-	failureFlash: false
-})(req, res, next);
-
-}
-
-exports.logout = (req, res) => {
-	req.logout();
-/* Logging out */
-	res.send("User Logout");
-}
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({ email: email })
+	  .then((user) => {
+		// console.log(user);
+		if (!user) {
+		  return res.json({ msg: "Incorrect E-Mail!!!" });
+		}
+		bcrypt
+		  .compare(password, user.password)
+		  .then((match) => {
+			console.log(match);
+			if (match) {
+			  // req.session.isLogin = true;
+			  req.session.user = user;
+			  res.json({ msg: "Login Successfully" });
+			}
+			if(!match){
+			  return res.json({ msg: "failed to login , Incorrect password!!!" });
+			}
+		  })
+		  .catch((err) => {
+			console.log(err);
+		  });
+	  })
+	  .catch((err) => {
+		console.log(err.msg);
+	  });
+  };
+  
+  exports.logout = (req, res, next) => {
+	req.session.destroy((err) => {
+	  // console.log(err);
+	  res.send({ msg: "Logout Successfully" });
+	});
+  };
+  
